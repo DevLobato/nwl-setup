58c191ee0dcccdfc13daefd06d7971caf67ff5ff
devLobato <dev.lobatovinicius@gmail.com>
2023-01-17 21:15:23 -0300

    Initiating project repo

---
+++ server/src/server.ts
@@ -0,0 +1,17 @@
+import Fastify from "fastify";
+import { PrismaClient} from "@prisma/client"
+
+const app = Fastify();
+const prisma = new PrismaClient()
+
+app.get("/habits", () =>{
+    const habits = prisma.habits.findMany()
+    
+    return habits
+})
+
+app.listen({
+    port: 7000,
+}).then(() => {
+    console.log("Server running!");
+})
